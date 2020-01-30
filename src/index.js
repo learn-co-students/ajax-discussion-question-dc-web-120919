907355c303b698135b258f13d8081daf393c0177
John Louis Rumingan <jmrumingan@gmail.com>
2020-01-30 14:02:58 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  let newPersonButton = document.getElementsByTagName('button')[0];
+
+  newPersonButton.addEventListener('click', pressButton())
+
 });
+
+
+function fetchPerson(){
+
+  return fetch('https://randomuser.me/api/')
+    .then(response => response.json());
+}
+
+function pressButton(){
+  alert("You've pressed the button!");
+}
